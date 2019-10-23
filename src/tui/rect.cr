b7c9f4487a5ed3c422c6960294ba31ec57e0eae4
Samual Black <samuallb@hotmail.com>
2019-10-23 09:53:29 +0100

    Relative rect helper

--- src/tui/rect.cr
+++ src/tui/rect.cr
@@ -8,4 +8,22 @@ struct TUI::Rect
     @x = 0
     @y = 0
   end
+
+  def relative(i, j = 0) : Rect
+    if i < 0 || j < 0
+      raise ArgumentError.new("Negative argument given #{i}, #{j}")
+    end
+    Rect.new(x+i, j+y, w-i, h-y)
+  end
+
+  def relative(i, j, e, r) : Rect
+    if i < 0 || j < 0 || e < 0 || r < 0
+      raise ArgumentError.new("Negative argument given #{i}, #{j}, #{e}, #{r}")
+    end
+    Rect.new(
+      i+x,
+      j+y,
+      Math.max(0, Math.min(e, w-i)),
+      Math.max(0, Math.min(r, y-j)))
+  end
 end
